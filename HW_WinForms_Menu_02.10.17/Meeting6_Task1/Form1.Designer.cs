7ab4ba7a858dcc4a342c0482d1f717f033ebbe67
AlisherB <alisher_b_95@mail.ru>
2017-10-08 00:15:48 +0600

    HW_WinForms_Menu_02.10.17
    Домашнее задание(Меню(Текстовый редактор))

---
+++ HW_WinForms_Menu_02.10.17/Meeting6_Task1/Form1.Designer.cs
@@ -0,0 +1,312 @@
+﻿namespace Meeting6_Task1
+{
+    partial class TextEditor
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TextEditor));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.createMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.undoMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.selectAllMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cutMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.copyMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pasteMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fontColorMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.backColorMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fontMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.fontDialog = new System.Windows.Forms.FontDialog();
+            this.colorDialog = new System.Windows.Forms.ColorDialog();
+            this.richTextBox = new System.Windows.Forms.RichTextBox();
+            this.contextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.copyContextMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cutContextMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pasteContextMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.undoContextMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.contextMenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileMenuItem,
+            this.editMenuItem,
+            this.viewMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(8, 2, 0, 2);
+            this.menuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            this.menuStrip1.Size = new System.Drawing.Size(909, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip";
+            // 
+            // fileMenuItem
+            // 
+            this.fileMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.createMenuItem,
+            this.openMenuItem,
+            this.saveAsMenuItem,
+            this.exitMenuItem});
+            this.fileMenuItem.Name = "fileMenuItem";
+            this.fileMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.fileMenuItem.Text = "Файл";
+            // 
+            // createMenuItem
+            // 
+            this.createMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("createMenuItem.Image")));
+            this.createMenuItem.Name = "createMenuItem";
+            this.createMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.createMenuItem.Text = "Создать";
+            this.createMenuItem.Click += new System.EventHandler(this.CreateMenuItem_Click);
+            // 
+            // openMenuItem
+            // 
+            this.openMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("openMenuItem.Image")));
+            this.openMenuItem.Name = "openMenuItem";
+            this.openMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openMenuItem.Text = "Открыть";
+            this.openMenuItem.Click += new System.EventHandler(this.OpenMenuItem_Click);
+            // 
+            // saveAsMenuItem
+            // 
+            this.saveAsMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("saveAsMenuItem.Image")));
+            this.saveAsMenuItem.Name = "saveAsMenuItem";
+            this.saveAsMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveAsMenuItem.Text = "Сохранить";
+            this.saveAsMenuItem.Click += new System.EventHandler(this.SaveAsMenuItem_Click);
+            // 
+            // exitMenuItem
+            // 
+            this.exitMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("exitMenuItem.Image")));
+            this.exitMenuItem.Name = "exitMenuItem";
+            this.exitMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitMenuItem.Text = "Выход";
+            this.exitMenuItem.Click += new System.EventHandler(this.ExitMenuItem_Click);
+            // 
+            // editMenuItem
+            // 
+            this.editMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.undoMenuItem,
+            this.selectAllMenuItem,
+            this.cutMenuItem,
+            this.copyMenuItem,
+            this.pasteMenuItem});
+            this.editMenuItem.Name = "editMenuItem";
+            this.editMenuItem.Size = new System.Drawing.Size(68, 20);
+            this.editMenuItem.Text = "Правка";
+            // 
+            // undoMenuItem
+            // 
+            this.undoMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("undoMenuItem.Image")));
+            this.undoMenuItem.Name = "undoMenuItem";
+            this.undoMenuItem.Size = new System.Drawing.Size(172, 22);
+            this.undoMenuItem.Text = "Отмена";
+            this.undoMenuItem.Click += new System.EventHandler(this.UndoMenuItem_Click);
+            // 
+            // selectAllMenuItem
+            // 
+            this.selectAllMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("selectAllMenuItem.Image")));
+            this.selectAllMenuItem.Name = "selectAllMenuItem";
+            this.selectAllMenuItem.Size = new System.Drawing.Size(172, 22);
+            this.selectAllMenuItem.Text = "Выделить все";
+            this.selectAllMenuItem.Click += new System.EventHandler(this.SelectAllMenuItem_Click);
+            // 
+            // cutMenuItem
+            // 
+            this.cutMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("cutMenuItem.Image")));
+            this.cutMenuItem.Name = "cutMenuItem";
+            this.cutMenuItem.Size = new System.Drawing.Size(172, 22);
+            this.cutMenuItem.Text = "Вырезать";
+            this.cutMenuItem.Click += new System.EventHandler(this.CutMenuItem_Click);
+            // 
+            // copyMenuItem
+            // 
+            this.copyMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("copyMenuItem.Image")));
+            this.copyMenuItem.Name = "copyMenuItem";
+            this.copyMenuItem.Size = new System.Drawing.Size(172, 22);
+            this.copyMenuItem.Text = "Копировать";
+            this.copyMenuItem.Click += new System.EventHandler(this.CopyMenuItem_Click);
+            // 
+            // pasteMenuItem
+            // 
+            this.pasteMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("pasteMenuItem.Image")));
+            this.pasteMenuItem.Name = "pasteMenuItem";
+            this.pasteMenuItem.Size = new System.Drawing.Size(172, 22);
+            this.pasteMenuItem.Text = "Вставить";
+            this.pasteMenuItem.Click += new System.EventHandler(this.PasteMenuItem_Click);
+            // 
+            // viewMenuItem
+            // 
+            this.viewMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fontColorMenuItem,
+            this.backColorMenuItem,
+            this.fontMenuItem});
+            this.viewMenuItem.Name = "viewMenuItem";
+            this.viewMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.viewMenuItem.Text = "Вид";
+            // 
+            // fontColorMenuItem
+            // 
+            this.fontColorMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("fontColorMenuItem.Image")));
+            this.fontColorMenuItem.Name = "fontColorMenuItem";
+            this.fontColorMenuItem.Size = new System.Drawing.Size(164, 22);
+            this.fontColorMenuItem.Text = "Цвет шрифта";
+            this.fontColorMenuItem.Click += new System.EventHandler(this.FontColorMenuItem_Click);
+            // 
+            // backColorMenuItem
+            // 
+            this.backColorMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("backColorMenuItem.Image")));
+            this.backColorMenuItem.Name = "backColorMenuItem";
+            this.backColorMenuItem.Size = new System.Drawing.Size(164, 22);
+            this.backColorMenuItem.Text = "Цвет фона";
+            this.backColorMenuItem.Click += new System.EventHandler(this.BackColorMenuItem_Click);
+            // 
+            // fontMenuItem
+            // 
+            this.fontMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("fontMenuItem.Image")));
+            this.fontMenuItem.Name = "fontMenuItem";
+            this.fontMenuItem.Size = new System.Drawing.Size(164, 22);
+            this.fontMenuItem.Text = "Шрифт";
+            this.fontMenuItem.Click += new System.EventHandler(this.FontMenuItem_Click);
+            // 
+            // richTextBox
+            // 
+            this.richTextBox.AcceptsTab = true;
+            this.richTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox.ContextMenuStrip = this.contextMenuStrip;
+            this.richTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox.EnableAutoDragDrop = true;
+            this.richTextBox.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.richTextBox.Location = new System.Drawing.Point(0, 24);
+            this.richTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.richTextBox.Name = "richTextBox";
+            this.richTextBox.Size = new System.Drawing.Size(909, 514);
+            this.richTextBox.TabIndex = 1;
+            this.richTextBox.Text = "";
+            // 
+            // contextMenuStrip
+            // 
+            this.contextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.copyContextMenuItem,
+            this.cutContextMenuItem,
+            this.pasteContextMenuItem,
+            this.undoContextMenuItem});
+            this.contextMenuStrip.Name = "contextMenuStrip";
+            this.contextMenuStrip.Size = new System.Drawing.Size(140, 92);
+            // 
+            // copyContextMenuItem
+            // 
+            this.copyContextMenuItem.Name = "copyContextMenuItem";
+            this.copyContextMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.copyContextMenuItem.Text = "Копировать";
+            this.copyContextMenuItem.Click += new System.EventHandler(this.CopyContextMenuItem_Click);
+            // 
+            // cutContextMenuItem
+            // 
+            this.cutContextMenuItem.Name = "cutContextMenuItem";
+            this.cutContextMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.cutContextMenuItem.Text = "Вырезать";
+            this.cutContextMenuItem.Click += new System.EventHandler(this.CutContextMenuItem_Click);
+            // 
+            // pasteContextMenuItem
+            // 
+            this.pasteContextMenuItem.Name = "pasteContextMenuItem";
+            this.pasteContextMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.pasteContextMenuItem.Text = "Вставить";
+            this.pasteContextMenuItem.Click += new System.EventHandler(this.PasteContextMenuItem_Click);
+            // 
+            // undoContextMenuItem
+            // 
+            this.undoContextMenuItem.Name = "undoContextMenuItem";
+            this.undoContextMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.undoContextMenuItem.Text = "Отменить";
+            this.undoContextMenuItem.Click += new System.EventHandler(this.UndoContextMenuItem_Click);
+            // 
+            // TextEditor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(909, 538);
+            this.Controls.Add(this.richTextBox);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "TextEditor";
+            this.Text = "Текстовый редактор";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.contextMenuStrip.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem createMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem undoMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cutMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem copyMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pasteMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fontColorMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem backColorMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fontMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+        private System.Windows.Forms.ToolStripMenuItem saveAsMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitMenuItem;
+        private System.Windows.Forms.FontDialog fontDialog;
+        private System.Windows.Forms.ColorDialog colorDialog;
+        private System.Windows.Forms.RichTextBox richTextBox;
+        private System.Windows.Forms.ToolStripMenuItem selectAllMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem copyContextMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cutContextMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pasteContextMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem undoContextMenuItem;
+    }
+}
+
---
+++ HW_WinForms_Menu_02.10.17/Meeting6_Task1/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Meeting6_Task1
+{
+    public partial class TextEditor : Form
+    {
+        public TextEditor()
+        {
+            InitializeComponent();
+            openFileDialog.Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*";
+            saveFileDialog.Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*";
+            fontDialog.ShowColor = true;
+            colorDialog.FullOpen = true;
+            colorDialog.Color = this.BackColor;
+        }
+
+        private string filename;
+        private string fileText;
+        private TextEditor textEditor;
+        private void OpenMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == DialogResult.Cancel)
+                return;
+            filename = openFileDialog.FileName;
+            fileText = File.ReadAllText(filename);
+            richTextBox.Text = fileText;
+        }
+        
+        private void SaveAsMenuItem_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog.ShowDialog() == DialogResult.Cancel)
+                return;
+            filename = saveFileDialog.FileName;
+            File.WriteAllText(filename, richTextBox.Text);
+        }
+
+        private void CreateMenuItem_Click(object sender, EventArgs e)
+        {
+            if (richTextBox.TextLength != 0)
+            {
+                DialogResult res = MessageBox.Show("Текстовый редактор содержит текст. Сохранить?", "Внимание", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (res == DialogResult.Yes)
+                {
+                    if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                        File.WriteAllText(saveFileDialog.FileName, richTextBox.Text);
+                }
+                else if (res == DialogResult.No)
+                    richTextBox.Clear();
+            }
+        }
+        
+        private void CopyMenuItem_Click(object sender, EventArgs e)
+        {
+            if (richTextBox.SelectionLength > 0)
+                richTextBox.Copy();
+        }
+        
+        private void CutMenuItem_Click(object sender, EventArgs e)
+        {
+            if (richTextBox.SelectedText != "")
+                richTextBox.Cut();
+        }
+
+        private void PasteMenuItem_Click(object sender, EventArgs e)
+        {
+            if (Clipboard.GetDataObject().GetDataPresent(DataFormats.Text) == true)
+            {
+                if (richTextBox.SelectionLength > 0)
+                     richTextBox.SelectionStart = richTextBox.SelectionStart + richTextBox.SelectionLength;
+                richTextBox.Paste();
+            }
+        }
+
+        private void UndoMenuItem_Click(object sender, EventArgs e)
+        {
+            if (richTextBox.CanUndo)
+            {
+                richTextBox.Undo();
+                richTextBox.ClearUndo();
+            }
+        }
+
+        private void ExitMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void FontColorMenuItem_Click(object sender, EventArgs e)
+        {
+            if (fontDialog.ShowDialog() == DialogResult.Cancel)
+                return;
+            
+            else if (richTextBox.SelectionLength > 0)
+                richTextBox.SelectionColor = fontDialog.Color;
+            else richTextBox.ForeColor = fontDialog.Color;
+        }
+
+        private void FontMenuItem_Click(object sender, EventArgs e)
+        {
+            if (fontDialog.ShowDialog() == DialogResult.Cancel)
+                return;
+            else if (richTextBox.SelectionLength > 0)
+                richTextBox.SelectionFont = fontDialog.Font;
+            else richTextBox.Font = fontDialog.Font;
+        }
+
+        private void BackColorMenuItem_Click(object sender, EventArgs e)
+        {
+            if (colorDialog.ShowDialog() == DialogResult.Cancel)
+                return;
+            richTextBox.BackColor = colorDialog.Color;
+        }
+
+        private void SelectAllMenuItem_Click(object sender, EventArgs e)
+        {
+            if (richTextBox.TextLength > 0)
+                richTextBox.SelectAll();
+        }
+
+        private void CopyContextMenuItem_Click(object sender, EventArgs e)
+        {
+            if (richTextBox.SelectionLength > 0)
+                richTextBox.Copy();
+        }
+
+        private void CutContextMenuItem_Click(object sender, EventArgs e)
+        {
+            if (richTextBox.SelectedText != "")
+                richTextBox.Cut();
+        }
+
+        private void PasteContextMenuItem_Click(object sender, EventArgs e)
+        {
+            if (Clipboard.GetDataObject().GetDataPresent(DataFormats.Text) == true)
+            {
+                if (richTextBox.SelectionLength > 0)
+                    richTextBox.SelectionStart = richTextBox.SelectionStart + richTextBox.SelectionLength;
+                richTextBox.Paste();
+            }
+        }
+
+        private void UndoContextMenuItem_Click(object sender, EventArgs e)
+        {
+            if (richTextBox.CanUndo)
+            {
+                richTextBox.Undo();
+                richTextBox.ClearUndo();
+            }
+        }
+    }
+}
